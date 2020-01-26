9f3f368129ea4c8a6ac9641042f30aa7d87a446c
Abhai Kollara <abhai.dilip@gmail.com>
2020-01-26 21:28:18 +0530

    Added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Universe"
+    user = {'username':"Abhai"}
+    posts = [
+        {
+            "author":{"username":"Boo"},
+            "body": "Meh"
+        },
+        {
+            "author":{"username":"Fooo"},
+            "body": "Moo"
+        }
+    ]
+    return render_template('index.html', title="Muhahah", user=user, posts=posts)
